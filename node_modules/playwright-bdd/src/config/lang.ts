b79baf122653f02d97328eb89b6de1f817667f17
wisdomamopho <67025792+wisdomamopho@users.noreply.github.com>
2024-12-15 20:46:13 +0000

    clean up

--- node_modules/playwright-bdd/src/config/lang.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Helpers for Cucumber language option.
- */
-
-export const LANG_EN = 'en';
-
-export function isEnglish(lang?: string) {
-  return !lang || lang === LANG_EN;
-}
--- node_modules/playwright-bdd/src/decorators.ts
+++
@@ -1,8 +0,0 @@
-import { Fixture } from './steps/decorators/fixture';
-import { createStepDecorator } from './steps/decorators/steps';
-
-export { Fixture };
-export const Given = createStepDecorator('Given');
-export const When = createStepDecorator('When');
-export const Then = createStepDecorator('Then');
-export const Step = createStepDecorator('Unknown');
